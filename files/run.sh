da12373c1a24772a08fbc278e3f30b1cb0a2dc92
Dereckson <dereckson@espace-win.org>
2015-06-07 20:56:04 +0200

    Fixed database check issue

--- files/run.sh
+++ files/run.sh
@@ -4,8 +4,8 @@
 if [ ! -f .initialized ]; then
 	# Checks if we need to create the database 
 	DB=`mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -B -N -e "SHOW DATABASES LIKE '$DB_NAME';" 2>/dev/null`
-	if [ $DB != "$DB_NAME" ]; then
-		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $DB_NAME; CREATE USER 'otrs'@'localhost' IDENTIFIED BY 'otrs'; GRANT ALL PRIVILEGES ON ${DB}.* TO 'otrs'@'localhost' WITH GRANT OPTION;"
+	if [ "$DB" != "$DB_NAME" ]; then
+		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE ${DB_NAME}; CREATE USER 'otrs'@'%' IDENTIFIED BY 'otrs'; GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'otrs'@'%' WITH GRANT OPTION;"
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-schema.mysql.sql
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-initial_insert.mysql.sql
 		/opt/otrs/bin/otrs.SetPassword.pl --agent root@localhost yt7OgnRPZ2a45g8viz8iP3fec5yBDUpsWoNS6X8jL65a7dK0z5u0EMGrd5m2cf5
