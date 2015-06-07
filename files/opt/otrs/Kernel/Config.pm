9eb37f9141795cd60f4d069907ec9c6655d86c85
Dereckson <dereckson@espace-win.org>
2015-06-07 16:59:02 +0200

    Cleaned OTRS configuration file

--- files/opt/otrs/Kernel/Config.pm
+++ files/opt/otrs/Kernel/Config.pm
@@ -29,45 +29,17 @@ sub Load {
     # database settings                                    #
     # ---------------------------------------------------- #
 
-    # The database host
     $Self->{DatabaseHost} = 'mysql';
-
-    # The database name
     $Self->{Database} = '%%DATABASE%%';
-
-    # The database user
     $Self->{DatabaseUser} = 'otrs';
-
-    # The password of database user. You also can use bin/otrs.CryptPassword.pl
-    # for crypted passwords
     $Self->{DatabasePw} = 'otrs';
-
-    # The database DSN for MySQL ==> more: "perldoc DBD::mysql"
     $Self->{DatabaseDSN} = "DBI:mysql:database=$Self->{Database};host=$Self->{DatabaseHost};";
-
-    # The database DSN for PostgreSQL ==> more: "perldoc DBD::Pg"
-    # if you want to use a local socket connection
-#    $Self->{DatabaseDSN} = "DBI:Pg:dbname=$Self->{Database};";
-    # if you want to use a TCP/IP connection
-#    $Self->{DatabaseDSN} = "DBI:Pg:dbname=$Self->{Database};host=$Self->{DatabaseHost};";
-    # if you have PostgresSQL 8.1 or earlier, activate the legacy driver with this line:
-#    $Self->{DatabasePostgresqlBefore82} = 1;
-
-    # The database DSN for Microsoft SQL Server - only supported if OTRS is
-    # installed on Windows as well
-#    $Self->{DatabaseDSN} = "DBI:ODBC:driver={SQL Server};Database=$Self->{Database};Server=$Self->{DatabaseHost},1433";
-
-    # The database DSN for Oracle ==> more: "perldoc DBD::oracle"
-#    $ENV{ORACLE_HOME} = '/u01/app/oracle/product/10.2.0/client_1';
-#    $ENV{NLS_DATE_FORMAT} = 'YYYY-MM-DD HH24:MI:SS';
-#    $ENV{NLS_LANG} = "american_america.utf8";
-
-#    $Self->{DatabaseDSN} = "DBI:Oracle:sid=OTRS;host=$Self->{DatabaseHost};port=1522;";
 
     # ---------------------------------------------------- #
     # fs root directory
     # ---------------------------------------------------- #
-        $Self->{Home} = '/opt/otrs/';
+
+    $Self->{Home} = '/opt/otrs/';
 
     # ---------------------------------------------------- #
     # insert your own config settings "here"               #
@@ -86,6 +58,7 @@ sub Load {
     $Self->{'DefaultCharset'}      = 'utf-8';
     $Self->{'AdminEmail'}          = 'root@localhost';
     $Self->{'Package::Timeout'}    = '120';
+
     # Misc
     $Self->{'Loader::Enabled::CSS'}  = 0;
     $Self->{'Loader::Enabled::JS'}   = 0;
@@ -94,6 +67,7 @@ sub Load {
       'Prio' => '100',
       'Show' => 'false'
     };
+
     # ---------------------------------------------------- #
 
     # ---------------------------------------------------- #
