1717110e77ecd1dedf846db40a64542fe283768d
Dereckson <dereckson@espace-win.org>
2015-06-07 20:59:14 +0200

    Rebuild the configuration only at first container launch

--- files/run.sh
+++ files/run.sh
@@ -8,12 +8,12 @@ if [ ! -f .initialized ]; then
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE ${DB_NAME}; CREATE USER 'otrs'@'%' IDENTIFIED BY 'otrs'; GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'otrs'@'%' WITH GRANT OPTION;"
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-schema.mysql.sql
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-initial_insert.mysql.sql
+		/opt/otrs/bin/otrs.RebuildConfig.pl
 		/opt/otrs/bin/otrs.SetPassword.pl --agent root@localhost yt7OgnRPZ2a45g8viz8iP3fec5yBDUpsWoNS6X8jL65a7dK0z5u0EMGrd5m2cf5
 		sed -i "s/%%DATABASE%%/${DB_NAME}/" /opt/otrs/Kernel/Config.pm
 	fi
 
 	# Tasks to execute at every container creation
-	/opt/otrs/bin/otrs.RebuildConfig.pl &
 	touch .initialized
 fi
 
