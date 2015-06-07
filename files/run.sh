e54e1fc747903cf008b8bdfa569a0714a1762bc0
Dereckson <dereckson@espace-win.org>
2015-06-07 17:15:19 +0200

    Set an arbitrary default password for initial user
    Login: root@localhost
    Passw: yt7OgnRPZ2a45g8viz8iP3fec5yBDUpsWoNS6X8jL65a7dK0z5u0EMGrd5m2cf5

--- files/run.sh
+++ files/run.sh
@@ -8,7 +8,7 @@ if [ ! -f .initialized ]; then
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $DB_NAME; CREATE USER 'otrs'@'localhost' IDENTIFIED BY 'otrs'; GRANT ALL PRIVILEGES ON ${DB}.* TO 'otrs'@'localhost' WITH GRANT OPTION;"
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-schema.mysql.sql
 		mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" $DB_NAME < /opt/otrs/scripts/database/otrs-initial_insert.mysql.sql
-		/opt/otrs/bin/otrs.SetPassword.pl --agent root@localhost root
+		/opt/otrs/bin/otrs.SetPassword.pl --agent root@localhost yt7OgnRPZ2a45g8viz8iP3fec5yBDUpsWoNS6X8jL65a7dK0z5u0EMGrd5m2cf5
 		sed -i "s/%%DATABASE%%/${DB_NAME}/" /opt/otrs/Kernel/Config.pm
 	fi
 
